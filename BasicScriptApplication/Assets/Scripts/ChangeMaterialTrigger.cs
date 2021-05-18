adedf11b6ca39d5fb9aaaa91a20df12368b33b19
Lily <s210559@students.aie.edu.au>
2021-05-18 19:56:49 +0930

    not working but material change trigger began

---
+++ BasicScriptApplication/Assets/Scripts/ChangeMaterialTrigger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public Material black;
+    public Material green;
+    public Material red;
+    public Material yellow;
+    public Material white;
+    public Material blue;
+
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collidedObject)
+    {
+        if (collidedObject.gameObject.tag == "Player")
+        {
+
+        }
+        if (player.GetComponent<Renderer>().material.name == black.name + " (Instance)")
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = black.name;
+        }
+        else if (player.GetComponent<Renderer>().material.name == red.name + " (Instance)")
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = red.name;
+        }
+        else if (player.GetComponent<Renderer>().material.name == green.name + " (Instance)")
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = green.name;
+        }
+        else if (player.GetComponent<Renderer>().material.name == blue.name + " (Instance)")
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = blue.name;
+        }
+        else if (player.GetComponent<Renderer>().material.name == yellow.name + " (Instance) ")
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = yellow.name;
+        }
+        else if (player.GetComponent<Renderer>().material.name == white.name + " (Instance) ") ;
+        {
+            this.gameObject.GetComponent<Renderer>().material.name = white.name;
+        }
+        
+
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
